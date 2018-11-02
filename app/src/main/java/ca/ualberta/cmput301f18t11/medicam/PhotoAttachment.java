01a7f08210bb5d1f8348db117752d9f8ab0ce88d
PangZixuan <43222441+PangZixuan@users.noreply.github.com>
2018-11-02 15:53:09 -0600

    Add files via upload

---
+++ app/src/main/java/ca/ualberta/cmput301f18t11/medicam/PhotoAttachment.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cmput301f18t11.medicam;
+
+import android.media.Image;
+
+public abstract class PhotoAttachment extends Attachment{
+    private Image photo;
+    private BodyLocation body_location;
+}
