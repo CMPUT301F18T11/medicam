4e42575c69eba8ca036034487b2c790e21611ce4
ZixuanPang <zpang@ualberta.ca>
2018-11-02 15:09:36 -0600

    Test cases update

--- app/src/main/java/ca/ualberta/cmput301f18t11/medicam/InstancePhoto.java
+++ app/src/main/java/ca/ualberta/cmput301f18t11/medicam/InstancePhoto.java
@@ -2,6 +2,31 @@ package ca.ualberta.cmput301f18t11.medicam;
 
 import android.media.Image;
 
+import java.util.Collection;
+import java.util.UUID;
+
 public class InstancePhoto extends PhotoAttachment{
+    private UUID instancePhotoUUID;
+    private Collection<Image> instanceImageCollection;
+
+    public InstancePhoto(){}
+
+
+    /*
+    public void addInstancePhotoImage (Image image){
+        // function for seeing wiether image size is good will be added later version.
+        boolean imageValidSize = true;
+        if (imageValidSize) {
+            instanceImageCollection.add(image);
+        }
+    }
+    */
+    public void setInstancePhotoUUID(UUID instancePhotoUUID) {
+        this.instancePhotoUUID = instancePhotoUUID;
+    }
+
+    public UUID getInstancePhotoUUID(){
+        return instancePhotoUUID;
+    }
 
 }
--- app/src/main/java/ca/ualberta/cmput301f18t11/medicam/LocationAttachment.java
+++ app/src/main/java/ca/ualberta/cmput301f18t11/medicam/LocationAttachment.java
@@ -2,15 +2,26 @@ package ca.ualberta.cmput301f18t11.medicam;
 
 import android.location.Location;
 
+import java.util.ArrayList;
+
 public class LocationAttachment extends Attachment {
     private Location location;
+    private ArrayList<Double>map_coordinates = new ArrayList<Double>(2);
 
 
     public boolean nearby(Location oc, double dist){
         return false;
     }
 
-    public Coordinate map_coordinates(){
-        return Coordinate
+    public ArrayList getMap_coordinates(){
+        return map_coordinates;
     }
+
+    public void setMap_coordinates(Double x,Double y){
+        this.map_coordinates.add(x);
+        this.map_coordinates.add(y);
+
+    }
+
+    public Location getLocation;
 }
