36a43417a674504993be0e67e85184ddb6027863
PangZixuan <43222441+PangZixuan@users.noreply.github.com>
2018-11-02 15:54:38 -0600

    Add files via upload

---
+++ app/src/test/java/ca/ualberta/cmput301f18t11/medicam/InstancePhotoUnitTest.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cmput301f18t11.medicam;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class InstancePhotoUnitTest {
+    @Test
+    public void getInstancePhotoUUID(){
+        UUID wanttedUUID = UUID.randomUUID();
+        InstancePhoto testInsta = new InstancePhoto();
+        testInsta.setInstancePhotoUUID(wanttedUUID);
+        UUID answerUUID = testInsta.getInstancePhotoUUID();
+        assertEquals(wanttedUUID,answerUUID);
+    }
+
+}
---
+++ app/src/test/java/ca/ualberta/cmput301f18t11/medicam/LocationAttachmentUnitTest.java
@@ -0,0 +1,35 @@
+package ca.ualberta.cmput301f18t11.medicam;
+
+import android.location.Location;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class LocationAttachmentUnitTest {
+
+    @Test
+    public void testNearby() {
+        LocationAttachment testLoca = new LocationAttachment();
+        boolean answer = false;
+        boolean espect;
+        Location oc = null;
+        espect = testLoca.nearby(oc,0.00000);
+        assertEquals(answer,espect);
+    }
+
+    @Test
+    public void testMap_coordinates() {
+        LocationAttachment testLoca = new LocationAttachment();
+        ArrayList<Double> answer = new ArrayList<Double>(2);
+        answer.add(0,0.00000000);
+        answer.add(1,1.11111111);
+        testLoca.setMap_coordinates(0.00000000,1.11111111);
+        ArrayList espect = testLoca.getMap_coordinates();
+        assertEquals(answer,espect);
+
+    }
+}
