4dcf8cb58c4f90c0159415c960f5a15327be25a5
Shouyang Zhou <s.zhou1511@gmail.com>
2018-11-08 17:58:40 -0700

    Inital Commmit

--- app/src/main/java/ca/ualberta/cmput301f18t11/medicam/MainActivity.java
+++ app/src/main/java/ca/ualberta/cmput301f18t11/medicam/MainActivity.java
@@ -3,11 +3,29 @@ package ca.ualberta.cmput301f18t11.medicam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
+        DroidClientConfig config = builder.build();
+
+        JestClientFactory factory = new JestClientFactory();
+        factory.setDroidClientConfig(config);
+
+
+        JestDroidClient client = (JestDroidClient) factory.getObject();
+
+
+
+
     }
 }
