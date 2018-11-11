c93065dc611d2d2cdce3aa724a4c06e6d6b276ad
Shouyang Zhou <s.zhou1511@gmail.com>
2018-11-10 17:20:47 -0700

    Reverted Patient to use UUIDs instead of strings Removed much of Persisted model now that the functionality is done via controllers.

--- app/src/main/java/ca/ualberta/cmput301f18t11/medicam/models/abstracts/PersistedModel.java
+++ app/src/main/java/ca/ualberta/cmput301f18t11/medicam/models/abstracts/PersistedModel.java
@@ -7,45 +7,11 @@ import java.util.UUID;
 
 public abstract class PersistedModel implements Serializable {
 
+    protected Boolean hasChangedOffline = false;
     protected String uuid = UUID.randomUUID().toString();
 
-    protected String stored_hash;
-
-    protected String index;
-
-    protected String type;
-
-    //These are mostly data access and storage methods.
-    // Should they be abstract?
-    public static PersistedModel load(){return null;/*idk what this does*/}
-    public void save(Context context){}
-    public void delete(){}
-    public static PersistedModel loadFromREST(){return null;/*query elastic search*/}
-    public static PersistedModel loadFromStorage(){return null;/*access internal storage*/}
-
-    public void saveToREST() {}
-    public void saveToStorage(Context context){}
-
-
-    public void deleteFromREST(){}
-    public void deleteFromStorage(){}
-
-    //get methods
-    public String getIndex(){ return index; }
-    public String getType(){ return type; }
     public String getUuid() { return uuid; }
-    public String getStored_hash() { return stored_hash; }
 
-    //set methods
-    public void setType(String type) { this.type = type; }
-    public void setIndex(String index) { this.index = index; }
-
-    //hash generation
-    public void createStored_hash() {
-        if (stored_hash.isEmpty()){
-            //do some hash generation algorithm
-        }
-    }
 
     public void createUuid() {
         if (uuid == null){
