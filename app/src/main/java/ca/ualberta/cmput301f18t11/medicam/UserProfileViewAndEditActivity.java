57b7114b9aff395864226cbe5559c2b3a393b06e
PangZixuan <zpang@ualberta.ca>
2018-11-13 21:23:31 -0700

    photo attaching method added

---
+++ app/src/main/java/ca/ualberta/cmput301f18t11/medicam/UserProfileViewAndEditActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cmput301f18t11.medicam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfileViewAndEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_view_and_edit);
+    }
+}
--- app/src/main/java/ca/ualberta/cmput301f18t11/medicam/createRecordActivity.java
+++ app/src/main/java/ca/ualberta/cmput301f18t11/medicam/createRecordActivity.java
@@ -1,22 +1,32 @@
 package ca.ualberta.cmput301f18t11.medicam;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.nfc.Tag;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.net.URI;
 import java.sql.Time;
 import java.util.Calendar;
 
 public class createRecordActivity extends AppCompatActivity {
+    private static final int OPEN_CAMERA_REQUEST_CODE = 0;
+    private static final int OPEN_GALLAY_REQUEST_CODE = 1;
 
     //Resource used for date picker: https://www.youtube.com/watch?v=hwe1abDO2Ag
     //Resource used for time picker: https://www.youtube.com/watch?v=QMwaNN_aM3U
@@ -25,12 +35,13 @@ public class createRecordActivity extends AppCompatActivity {
     private TextView displayTime;
     private DatePickerDialog.OnDateSetListener mDate;
     private TimePickerDialog.OnTimeSetListener mTime;
+    private ImageView photoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_record);
-
+        photoImageView = findViewById(R.id.recorePhotoImageView);
         displayDate = (TextView) findViewById(R.id.dateView);
         displayTime = (TextView) findViewById(R.id.timeView);
 
@@ -69,6 +80,33 @@ public class createRecordActivity extends AppCompatActivity {
             }
         };
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == OPEN_CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Log.d("SELECT PHOTO DIALOG","onActivityResult: done takeing new photo");
+            Bitmap bitmap;
+            bitmap = (Bitmap) data.getExtras().get("data");
+            photoImageView.setImageBitmap(bitmap);
+        }
+
+        else if(requestCode == OPEN_GALLAY_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Log.d("SELECT PHOTO DIALOG","onActivityResult: done SELECTING new photo");
+            Uri selectedImageUri = data.getData();
+            photoImageView.setImageURI(selectedImageUri);
+        }
+    }
+
+    public void goToCamera(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,OPEN_CAMERA_REQUEST_CODE);
+    }
+    public void goToGallary(View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent,OPEN_GALLAY_REQUEST_CODE);
     }
 
 }
