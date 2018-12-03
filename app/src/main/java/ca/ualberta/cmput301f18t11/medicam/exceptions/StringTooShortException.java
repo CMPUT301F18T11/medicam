1d5829a76a39cd24995a9d3293e16f8549bdd629
shouyang <shouyang@ualberta.ca>
2018-12-03 12:06:27 -0700

    Added Comments to All Exceptions

--- app/src/main/java/ca/ualberta/cmput301f18t11/medicam/exceptions/StringTooShortException.java
+++ app/src/main/java/ca/ualberta/cmput301f18t11/medicam/exceptions/StringTooShortException.java
@@ -1,4 +1,7 @@
 package ca.ualberta.cmput301f18t11.medicam.exceptions;
 
+/**
+ *  StringLengthTooLongException to be thrown on string fields where a min length is present and exceeded.
+ */
 public class StringTooShortException extends RuntimeException {
 }
